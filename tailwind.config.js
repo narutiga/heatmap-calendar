49155ee32c495f0c3a4a5c048e8fa33f59e19ba4
narutiga <100295602+narutiga@users.noreply.github.com>
2023-03-22 21:54:22 +0900

    🎉 Begin a project.

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: ["./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false,
+  theme: {
+    extend: {
+      backgroundColor: {
+        "heatmap-pink": {
+          100: "#ffe0e6",
+          200: "#ffb3c2",
+          300: "#ff809c",
+          400: "#ff4d75",
+          500: "#ff1a4d",
+        },
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
