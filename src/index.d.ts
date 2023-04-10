55644764f2c692eae2abc1da8a1722841fef9896
narutiga <100295602+narutiga@users.noreply.github.com>
2023-04-10 21:22:02 +0900

    🩹 correct type definition.

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,25 +1,13 @@
 import { FC } from "react";
 
-const DEFAULT_CONTAINER_SIZE = "50rem";
-
-const DEFAULT_COLOR_LEVELS = [
-  "bg-gray-50",
-  "bg-rose-100",
-  "bg-rose-200",
-  "bg-rose-300",
-  "bg-rose-400",
-];
-
-const DEFAULT_COLOR_RANGE_STEP = 10;
-
 export type HeatmapProps = {
   value: {
     date: string;
     count: number;
   }[];
-  containerSize: string = DEFAULT_CONTAINER_SIZE;
-  colorLevels: string[] = DEFAULT_COLOR_LEVELS;
-  colorRangeStep: number = DEFAULT_COLOR_RANGE_STEP;
+  containerSize?: string;
+  colorLevels?: string[];
+  colorRangeStep?: number;
 };
 
 export type HeatmapComponent = FC<HeatmapProps>;
